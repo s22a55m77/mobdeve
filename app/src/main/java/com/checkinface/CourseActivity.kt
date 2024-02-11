bb1962175a4edcbb93d7ddb0c6ad170d8ba928ce
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-02-11 14:51:43 +0800

    remove unused code

--- app/src/main/java/com/checkinface/CourseActivity.kt
+++ app/src/main/java/com/checkinface/CourseActivity.kt
@@ -6,12 +6,8 @@ import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
 
 class CourseActivity : AppCompatActivity() {
-    lateinit var tv_appbar: MaterialToolbar;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course)
-
-        this.tv_appbar = findViewById(R.id.appbar_top)
-        tv_appbar.title = "Course"
     }
 }
