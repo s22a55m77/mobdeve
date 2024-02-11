f29ec8686f1bd6317220e84d7595c6c40c4db562
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-02-11 12:27:26 +0800

    add initialize activity

--- app/src/main/java/com/checkinface/MainActivity.kt
+++ app/src/main/java/com/checkinface/MainActivity.kt
@@ -3,10 +3,14 @@ package com.checkinface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.checkinface.dashboard.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, InitializeActivity::class.java)
+        startActivity(intent)
     }
 }
