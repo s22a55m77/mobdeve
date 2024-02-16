20066b8b49c3b1eb4d169b4bba4884ebd001398d
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-02-16 11:28:23 +0800

    add create course

--- app/src/main/java/com/checkinface/fragment/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/checkinface/fragment/dashboard/DashboardFragment.kt
@@ -1,18 +1,35 @@
 package com.checkinface.fragment.dashboard
 
+import android.app.AlertDialog
+import android.content.DialogInterface.OnShowListener
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.checkinface.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
 
 class DashboardFragment : Fragment() {
     private val dashboardModelList = DashboardDataGenerator.loadData()
     private lateinit var recyclerView: RecyclerView
+    private lateinit var btnAddCourse: ExtendedFloatingActionButton
+    private lateinit var ivQrCode: ImageView
+    private lateinit var edAddCourse: EditText
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -23,6 +40,51 @@ class DashboardFragment : Fragment() {
         this.recyclerView.layoutManager = gridLayoutManager
 
         this.recyclerView.adapter = DashboardAdapter(this.dashboardModelList)
+
+        // Course QR Code Dialog
+        val qrModalView = layoutInflater.inflate(R.layout.qr_code_layout, null)
+        this.ivQrCode = qrModalView.findViewById(R.id.iv_qr_code_container)
+        fun generateQR() {
+            val text = "Test"
+            val writer = MultiFormatWriter()
+            try {
+                val matrix: BitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 400, 400)
+                val encoder = BarcodeEncoder()
+                val bitmap = encoder.createBitmap(matrix)
+                ivQrCode.setImageBitmap(bitmap)
+            } catch (e: WriterException) {
+                Log.e("QR Code", "Error in creating QR code")
+            }
+        }
+        val qrDialog = MaterialAlertDialogBuilder(this.requireContext()).setView(qrModalView)
+        val qrModal = qrDialog.create()
+
+        // Add Course Dialog
+        val modalView = layoutInflater.inflate(R.layout.create_course, null)
+        this.btnAddCourse = view.findViewById(R.id.btn_teacher_add_course)
+        this.edAddCourse = modalView.findViewById(R.id.ed_add_course_name)
+        val dialogBuilder = MaterialAlertDialogBuilder(this.requireContext())
+            .setView(modalView)
+            .setTitle("Add Course")
+            .setNegativeButton("Cancel") { dialog, which ->
+                dialog.cancel()
+            }
+            .setPositiveButton("Next") { dialog, which ->
+                if(edAddCourse.text.isNullOrEmpty()) {
+                    Toast.makeText(this.context, "Please enter the course name", Toast.LENGTH_LONG).show()
+                }
+                else {
+                    dialog.cancel()
+                    generateQR()
+                    qrModal.show()
+                }
+            }
+        val addCourseModal = dialogBuilder.create()
+
+        // Button Click
+        btnAddCourse.setOnClickListener {
+            addCourseModal.show()
+        }
     }
 
     override fun onCreateView(
