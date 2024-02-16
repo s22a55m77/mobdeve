6c24704dd9b4d74e27951d1ccb93899ed4870278
s22a55m77 <s22a55m77@gmail.com>
2024-02-16 11:14:56 +0800

    fix data bind

--- app/src/main/java/com/checkinface/fragment/teacher_course/student_list/StudentListViewHolder.kt
+++ app/src/main/java/com/checkinface/fragment/teacher_course/student_list/StudentListViewHolder.kt
@@ -1,4 +1,4 @@
-package com.checkinface.fragment.teacher_course
+package com.checkinface.fragment.teacher_course.student_list
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.checkinface.databinding.StudentItemLayoutBinding
@@ -6,8 +6,8 @@ import com.checkinface.databinding.StudentItemLayoutBinding
 class StudentListViewHolder(private val binding: StudentItemLayoutBinding): ViewHolder(binding.root) {
     fun bindData(studentModel: StudentModel) {
         binding.tvStudentName.text = studentModel.name
-        binding.tvStudentPresent.text = "Present: ${studentModel.presentCount}"
-        binding.tvStudentAbsent.text = "Absent: ${studentModel.absentCount}"
-        binding.tvStudentLate.text = "Late: ${studentModel.lateCount}"
+        binding.tvStudentPresent.text = studentModel.presentCount.toString()
+        binding.tvStudentAbsent.text = studentModel.absentCount.toString()
+        binding.tvStudentLate.text = studentModel.lateCount.toString()
     }
 }
