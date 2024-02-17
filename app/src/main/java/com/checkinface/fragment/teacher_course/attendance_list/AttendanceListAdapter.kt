a6ddc5b1428d3d2a9cb6a303503c34780cd8175a
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-02-17 09:12:35 +0800

    add qr display

--- app/src/main/java/com/checkinface/fragment/teacher_course/attendance_list/AttendanceListAdapter.kt
+++ app/src/main/java/com/checkinface/fragment/teacher_course/attendance_list/AttendanceListAdapter.kt
@@ -1,5 +1,6 @@
 package com.checkinface.fragment.teacher_course.attendance_list
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -9,8 +10,14 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.checkinface.R
 import com.checkinface.databinding.AttendanceItemLayoutBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class AttendanceListAdapter(private val data: ArrayList<TeacherAttendanceModel>): Adapter<AttendanceListViewHolder>() {
+    private lateinit var ivQrCode: ImageView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AttendanceListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = AttendanceItemLayoutBinding.inflate(inflater, parent, false)
@@ -33,7 +40,25 @@ class AttendanceListAdapter(private val data: ArrayList<TeacherAttendanceModel>)
 
         val qrcode = holder.itemView.findViewById<ImageView>(R.id.iv_attendance_item_qr_code)
         qrcode.setOnClickListener {
-            
+            val layoutInflater = LayoutInflater.from(holder.itemView.context)
+            val qrModalView = layoutInflater.inflate(R.layout.qr_code_layout, null)
+            this.ivQrCode = qrModalView.findViewById(R.id.iv_qr_code_container)
+            fun generateQR() {
+                val text = "Test"
+                val writer = MultiFormatWriter()
+                try {
+                    val matrix: BitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 400, 400)
+                    val encoder = BarcodeEncoder()
+                    val bitmap = encoder.createBitmap(matrix)
+                    this.ivQrCode.setImageBitmap(bitmap)
+                } catch (e: WriterException) {
+                    Log.e("QR Code", "Error in creating QR code")
+                }
+            }
+            val qrDialog = MaterialAlertDialogBuilder(holder.itemView.context).setView(qrModalView)
+            val qrModal = qrDialog.create()
+            generateQR()
+            qrModal.show()
         }
     }
 }
