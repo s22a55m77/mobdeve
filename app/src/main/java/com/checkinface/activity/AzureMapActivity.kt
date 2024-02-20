e33dc6920957aa452b40662c256e8bd0fbd0abeb
s22a55m77 <s22a55m77@gmail.com>
2024-02-20 12:44:12 +0800

    add map function

--- app/src/main/java/com/checkinface/activity/AzureMapActivity.kt
+++ app/src/main/java/com/checkinface/activity/AzureMapActivity.kt
@@ -1,29 +1,74 @@
 package com.checkinface.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.azure.android.maps.control.AzureMap
 import com.azure.android.maps.control.AzureMaps
 import com.azure.android.maps.control.MapControl
+import com.azure.android.maps.control.controls.ZoomControl
+import com.azure.android.maps.control.events.OnClick
 import com.azure.android.maps.control.events.OnReady
+import com.azure.android.maps.control.layer.SymbolLayer
+import com.azure.android.maps.control.source.DataSource
 import com.checkinface.R
+import com.checkinface.databinding.ActivityAzureMapBinding
+import com.mapbox.geojson.Point
 
 class AzureMapActivity : AppCompatActivity() {
     companion object {
+        const val LON_KEY = "LON_KEY"
+        const val LAT_KEY = "LAT_KEY"
         init {
             AzureMaps.setSubscriptionKey("4AWaaMcQhk29a8NB3UBEA_By3uT_FICEMAZdOiZqjc8");
         }
     }
+    private var symbolLayer: SymbolLayer? = null
+    private var lon: Double? = null
+    private var lat: Double? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_azure_map)
+        val viewBinding = ActivityAzureMapBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         mapControl = findViewById(R.id.azure_map_control)
 
         mapControl?.onCreate(savedInstanceState)
 
         //Wait until the map resources are ready.
-        mapControl?.onReady(OnReady { map: AzureMap -> })
+        mapControl?.onReady(OnReady { map: AzureMap ->
+            map.controls.add(ZoomControl())
+
+            val source = DataSource()
+            map.sources.add(source)
+
+            map.events.add(OnClick { lat: Double, lon: Double ->
+                symbolLayer?.let { map.layers.remove(it) }
+
+                source.clear()
+                //Map clicked.
+                source.add(Point.fromLngLat(lon, lat))
+                this.lon = lon
+                this.lat = lat
+                symbolLayer = SymbolLayer(source)
+                map.layers.add(symbolLayer)
+
+                //Return true indicating if event should be consumed and not passed further to other listeners registered afterwards, false otherwise.
+                return@OnClick false
+            })
+
+        })
+
+        viewBinding.btnSetLocation.setOnClickListener{
+            val returnIntent = Intent()
+            returnIntent.putExtra(LAT_KEY, lat)
+            returnIntent.putExtra(LON_KEY, lon)
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
+        }
     }
 
     var mapControl: MapControl? = null
