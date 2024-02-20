05dec363dc43c06812a6311adbe0d154c3cb598f
s22a55m77 <s22a55m77@gmail.com>
2024-02-20 09:31:55 +0800

    add modal

--- app/src/main/java/com/checkinface/fragment/student_attendance_list/StudentAttendanceListAdapter.kt
+++ app/src/main/java/com/checkinface/fragment/student_attendance_list/StudentAttendanceListAdapter.kt
@@ -3,7 +3,9 @@ package com.checkinface.fragment.student_attendance_list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.checkinface.R
 import com.checkinface.databinding.CouseItemLayoutBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class StudentAttendanceListAdapter(private val data: ArrayList<StudentAttendanceModel>): Adapter<StudentAttendanceListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentAttendanceListViewHolder {
@@ -19,6 +21,13 @@ class StudentAttendanceListAdapter(private val data: ArrayList<StudentAttendance
 
     override fun onBindViewHolder(holder: StudentAttendanceListViewHolder, position: Int) {
         holder.bindData(data[position])
+
+        holder.itemView.setOnClickListener {
+            val layoutInflater = LayoutInflater.from(holder.itemView.context)
+            val modalView = layoutInflater.inflate(R.layout.modify_student_attendance_modal_layout, null)
+            val modal = MaterialAlertDialogBuilder(holder.itemView.context).setView(modalView)
+            modal.show()
+        }
     }
 
 }
--- app/src/main/java/com/checkinface/fragment/teacher_course/attendance_detail_student_list/AttendanceDetailStudentListAdapter.kt
+++ app/src/main/java/com/checkinface/fragment/teacher_course/attendance_detail_student_list/AttendanceDetailStudentListAdapter.kt
@@ -3,7 +3,9 @@ package com.checkinface.fragment.teacher_course.attendance_detail_student_list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.checkinface.R
 import com.checkinface.databinding.AttendanceDetailStudentItemLayoutBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class AttendanceDetailStudentListAdapter(private val data: ArrayList<AttendanceDetailStudentModel>): Adapter<AttendanceDetailStudentListViewHolder>() {
     override fun onCreateViewHolder(
@@ -22,5 +24,13 @@ class AttendanceDetailStudentListAdapter(private val data: ArrayList<AttendanceD
 
     override fun onBindViewHolder(holder: AttendanceDetailStudentListViewHolder, position: Int) {
         holder.bindData(data[position])
+
+        holder.itemView.setOnClickListener {
+            val layoutInflater = LayoutInflater.from(holder.itemView.context)
+            val modalView = layoutInflater.inflate(R.layout.modify_student_attendance_modal_layout, null)
+            val modal = MaterialAlertDialogBuilder(holder.itemView.context).setView(modalView)
+            modal.show()
+
+        }
     }
 }
