ea5f02a96a83777d814b0ac79df730ce067816c7
s22a55m77 <s22a55m77@gmail.com>
2024-02-20 09:39:01 +0800

    add more data

--- app/src/main/java/com/checkinface/fragment/student_attendance_list/StudentAttendanceDataGenerator.kt
+++ app/src/main/java/com/checkinface/fragment/student_attendance_list/StudentAttendanceDataGenerator.kt
@@ -14,25 +14,73 @@ class StudentAttendanceDataGenerator {
             )
             data.add(
                 StudentAttendanceModel(
-                    DateUtil.getDate("2024-02-12T07:00:01+08:00"),
+                    DateUtil.getDate("2024-02-13T07:00:01+08:00"),
                     AttendanceStatus.ABSENT
                 )
             )
             data.add(
                 StudentAttendanceModel(
-                    DateUtil.getDate("2024-02-12T07:00:01+08:00"),
+                    DateUtil.getDate("2024-02-14T07:00:01+08:00"),
                     AttendanceStatus.LATE
                 )
             )
             data.add(
                 StudentAttendanceModel(
-                    DateUtil.getDate("2024-02-12T07:00:01+08:00"),
+                    DateUtil.getDate("2024-02-16T07:00:01+08:00"),
                     AttendanceStatus.PRESENT
                 )
             )
             data.add(
                 StudentAttendanceModel(
-                    DateUtil.getDate("2024-02-12T07:00:01+08:00"),
+                    DateUtil.getDate("2024-02-18T07:00:01+08:00"),
+                    AttendanceStatus.PRESENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-19T07:00:01+08:00"),
+                    AttendanceStatus.PRESENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-20T07:00:01+08:00"),
+                    AttendanceStatus.PRESENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-21T07:00:01+08:00"),
+                    AttendanceStatus.ABSENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-22T07:00:01+08:00"),
+                    AttendanceStatus.PRESENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-23T07:00:01+08:00"),
+                    AttendanceStatus.ABSENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-24T07:00:01+08:00"),
+                    AttendanceStatus.LATE
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-25T07:00:01+08:00"),
+                    AttendanceStatus.PRESENT
+                )
+            )
+            data.add(
+                StudentAttendanceModel(
+                    DateUtil.getDate("2024-02-26T07:00:01+08:00"),
                     AttendanceStatus.PRESENT
                 )
             )
