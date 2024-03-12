3bbb0cb344a402e16299dc24e8ad470706b88b65
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-03-12 12:39:26 +0800

    fix date show style

--- app/src/main/java/com/checkinface/fragment/teacher_course/attendance_list/AttendanceListViewHolder.kt
+++ app/src/main/java/com/checkinface/fragment/teacher_course/attendance_list/AttendanceListViewHolder.kt
@@ -7,7 +7,7 @@ import com.google.android.gms.common.util.DataUtils
 
 class AttendanceListViewHolder(private val binding: AttendanceItemLayoutBinding): ViewHolder(binding.root) {
     fun bindData(attendanceModel: TeacherAttendanceModel) {
-        binding.tvAttendanceDate.text = DateUtil.getFormattedDate(attendanceModel.date)
+        binding.tvAttendanceDate.text = DateUtil.getFormattedDate("MMM d, yyyy HH:mm", attendanceModel.date)
         binding.tvAttendanceStudentPresent.text = attendanceModel.presentCount.toString()
         binding.tvAttendanceStudentAbsent.text = attendanceModel.absentCount.toString()
         binding.tvAttendanceStudentLate.text = attendanceModel.lateCount.toString()
