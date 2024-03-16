d9d6a5eb19932e232b02f1f74a237ec599a0aa7c
s22a55m77 <s22a55m77@gmail.com>
2024-03-16 10:26:32 +0800

    fix scanner

--- app/src/main/java/com/checkinface/fragment/camera/CameraFragment.kt
+++ app/src/main/java/com/checkinface/fragment/camera/CameraFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.checkinface.R
+import com.google.android.gms.common.moduleinstall.ModuleInstall
+import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
@@ -15,15 +17,44 @@ import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val optional = GmsBarcodeScanning.getClient(requireContext())
+
+        val client = ModuleInstall.getClient(requireContext())
+        client.areModulesAvailable(optional)
+            .addOnSuccessListener { result ->
+                if (result.areModulesAvailable()) {
+                    // start scanner if the scanner module is already installed
+                    startScan()
+                } else {
+                    // install first before starting the scanner
+                    val installRequest = ModuleInstallRequest.newBuilder()
+                        .addApi(optional)
+                        .build()
+                    client.installModules(installRequest)
+                        .addOnFailureListener { exception ->
+                            Log.d("FAIL", exception.stackTraceToString())
+                        }
+                        .addOnSuccessListener {
+                            startScan()
+                        }
+                }
+            }
+
+
+    }
+
+    fun startScan() {
         val options = GmsBarcodeScannerOptions.Builder()
             .setBarcodeFormats(
                 Barcode.FORMAT_QR_CODE)
             .enableAutoZoom()
             .build()
 
-        val scanner = GmsBarcodeScanning.getClient(requireActivity().applicationContext, options)
-
+        val scanner = GmsBarcodeScanning.getClient(requireContext(), options)
         scanner.startScan()
+            .addOnFailureListener { exception ->
+                Log.d("FAIL", exception.stackTraceToString())
+            }
             .addOnSuccessListener { barcode ->
                 barcode.rawValue?.let { Log.d("CameraFragment", it) }
             }
