fce6966318596dbbaacd235355a42bf05782583a
s22a55m77 <s22a55m77@gmail.com>
2024-03-16 10:08:27 +0800

    add notification setting

--- app/src/main/java/com/checkinface/fragment/user_profile/UserProfileFragment.kt
+++ app/src/main/java/com/checkinface/fragment/user_profile/UserProfileFragment.kt
@@ -1,14 +1,13 @@
 package com.checkinface.fragment.user_profile
 
-import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -16,6 +15,7 @@ import com.checkinface.R
 import com.checkinface.activity.LoginActivity
 import com.checkinface.databinding.FragmentUserProfileBinding
 import com.checkinface.util.FirestoreUserHelper
+import com.checkinface.util.SettingSharedPreference
 import com.checkinface.util.UserRole
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -24,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.launch
+
 
 class UserProfileFragment : Fragment() {
     private lateinit var viewBinding: FragmentUserProfileBinding
@@ -35,22 +36,6 @@ class UserProfileFragment : Fragment() {
         val inflater = TransitionInflater.from(requireContext())
         enterTransition = inflater.inflateTransition(R.transition.slide)
     }
-
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // feature requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-            }
-        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -79,13 +64,26 @@ class UserProfileFragment : Fragment() {
             }
         }
 
+        val settingSharedPreference = SettingSharedPreference(requireContext())
+
+        viewBinding.switchUserProfileNotification.isChecked = settingSharedPreference.isNotification()
+
         viewBinding.switchUserProfileNotification.setOnClickListener {
 
             val isAllowedNotification = NotificationManagerCompat.from(requireContext()).areNotificationsEnabled()
             if (!isAllowedNotification) {
-                requestPermissionLauncher.launch(
-                    Manifest.permission.ACCESS_NOTIFICATION_POLICY)
-//                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY), 123)
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", context?.packageName!!, null)
+                intent.setData(uri);
+                startActivity(intent);
+                viewBinding.switchUserProfileNotification.isChecked = false
+            } else {
+
+                if (viewBinding.switchUserProfileNotification.isChecked) {
+                    settingSharedPreference.setNotification(true)
+                } else {
+                    settingSharedPreference.setNotification(false)
+                }
 
             }
 
